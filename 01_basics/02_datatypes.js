39972d6968738412ac341ca1ddf98a37fa8f4fdb
d-anchu <dhruvjordanair8@gmail.com>
2025-08-20 04:04:32 +0000

    converion of datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer verson 
+
+//alert(3 + 3) we using node.js
+
+console.log(3+3);
+console.log("Hitesh")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false 
+
+//number  => 2 to power 53 
+//bigint 
+// null =>standalone value 
+// undefined 
---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,17 @@
+let score = "33"
+let invalue = Number(score)
+
+console.log(typeof(score));
+
+console.log(typeof(invalue));
+console.log(invalue)
+
+// "33" => 33
+//"33abc" => Nan
+// true = 1 
+//false = 0
+
+// 1 =>true 
+// 0 =false 
+//"hitesh" = true
+//""
